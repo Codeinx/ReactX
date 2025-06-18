0faacaa669bb810adcbac5a6bd9a5295f5e5bb4f
Dominion Majemu-Itura Aseyege <98827618+Codeinx@users.noreply.github.com>
2025-06-18 04:10:11 +0000

    Day 1 of 100

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./styles.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    // console.log(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        This is advice number <strong>{count}</strong>
+      </p>
     </div>
   );
 }
